5c01a6d489f7c6fc4420f2ddd1f68801f58f2ea9
Minju Kang <minju.kang@students.olin.edu>
2018-03-02 11:18:36 -0500

    update

--- client4.py
+++ client4.py
@@ -2,18 +2,18 @@
 import socket
 import select
 import sys
- 
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-IP_address = '10.7.92.39' # '10.7.24.67'#str(sys.argv[1])
-Port = 5000 #int(sys.argv[2])
+IP_address = '10.7.27.237'  # '10.7.24.67'#str(sys.argv[1])
+Port = 5000  # int(sys.argv[2])
 server.connect((IP_address, Port))
- 
+
 while True:
- 
+
     # maintains a list of possible input streams
     sockets_list = [sys.stdin, server]
- 
+
     """ There are two possible input situations. Either the
     user wants to give  manual input to send to other people,
     or the server is sending a message  to be printed on the
@@ -22,8 +22,8 @@ while True:
     to send a message, then the if condition will hold true
     below.If the user wants to send a message, the else
     condition will evaluate as true"""
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
- 
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+
     for socks in read_sockets:
         if socks == server:
             message = socks.recv(2048)
--- server4.py
+++ server4.py
@@ -3,7 +3,7 @@ import socket
 import select
 import sys
 from _thread import *
- 
+
 """The first argument AF_INET is the address domain of the
 socket. This is used when we have an Internet Domain with
 any two hosts The second argument is the type of socket.
@@ -11,94 +11,102 @@ SOCK_STREAM means that data or characters are read in
 a continuous flow."""
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
- 
 
-IP_address = '10.7.92.39'   #'10.7.24.67'#str(sys.argv[1])
- 
+
+IP_address = '10.7.27.237'   # '10.7.24.67'#str(sys.argv[1])
+
 # takes second argument from command prompt as port number
-Port = 5000 
- 
+Port = 5000
+
 """
 binds the server to an entered IP address and at the
 specified port number.
 The client must be aware of these parameters
 """
 server.bind((IP_address, Port))
- 
+
 """
 listens for 100 active connections. This number can be
 increased as per convenience.
 """
 server.listen(100)
- 
+
 list_of_clients = []
- 
+
+
 def clientthread(conn, addr):
- 
+
     # sends a message to the client whose user object is conn
     conn.send(str.encode("Welcome to this chatroom!"))
- 
+
     while True:
             try:
                 message = conn.recv(2048)
                 if message:
- 
+
                     """prints the message and address of the
                     user who just sent the message on the server
                     terminal"""
                     print("<" + addr[0] + "> " + message)
- 
+
                     # Calls broadcast function to send message to all
                     message_to_send = "<" + addr[0] + "> " + message
                     broadcast(message_to_send, conn)
- 
+
                 else:
                     """message may have no content if the connection
                     is broken, in this case we remove the connection"""
                     remove(conn)
- 
+
             except:
                 continue
- 
+
+
 """Using the below function, we broadcast the message to all
 clients who's object is not the same as the one sending
 the message """
+
+
 def broadcast(message, connection):
     for clients in list_of_clients:
-        if clients!=connection:
+        if clients != connection:
             try:
                 clients.send(message)
             except:
                 clients.close()
- 
+
                 # if the link is broken, we remove the client
                 remove(clients)
- 
+
+
 """The following function simply removes the object
-from the list that was created at the beginning of 
+from the list that was created at the beginning of
 the program"""
+
+
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
- 
+
+
 while True:
- 
-    """Accepts a connection request and stores two parameters, 
-    conn which is a socket object for that user, and addr 
-    which contains the IP address of the client that just 
+
+    """Accepts a connection request and stores two parameters,
+    conn which is a socket object for that user, and addr
+    which contains the IP address of the client that just
     connected"""
     conn, addr = server.accept()
- 
+
     """Maintains a list of clients for ease of broadcasting
     a message to all available people in the chatroom"""
     list_of_clients.append(conn)
- 
+
     # prints the address of the user that just connected
     print(addr[0] + " connected")
- 
-    # creates and individual thread for every user 
+
+    # creates and individual thread for every user
     # that connects
-    start_new_thread(clientthread,(conn,addr))    
- 
+    start_new_thread(clientthread, (conn, addr))
+
 conn.close()
 server.close()
